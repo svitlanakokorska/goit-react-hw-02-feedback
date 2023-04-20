e71f42af53ba73a53c8f8d9db9154a561e8ec7df
svitlanakokorska <s.kokorskaya@gmail.com>
2023-04-20 17:12:57 +0300

    завдання1

---
+++ src/components/Section/Section.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Section = ({ title, children }) => {
+  return (
+    <>
+      <h2>{title}</h2>
+      {children}
+    </>
+  );
+};
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+};
